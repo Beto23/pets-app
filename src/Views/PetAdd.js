0debb40707ea58395dc77e40eb68479803e928dc
beto <jooseplugo@gmail.com>
2017-07-27 15:04:34 -0500

    Add image piker react-native-image-picker was added

--- src/Views/PetAdd.js
+++ src/Views/PetAdd.js
@@ -1,16 +1,64 @@
+//Dependencies
 import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    Image,
+    StyleSheet,
+    TouchableHighlight
 } from 'react-native'
+
+var ImagePicker = require('react-native-image-picker');
 
 class PetAdd extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            imagePath: '',
+            imageHeight: '',
+            imageWidth: ''
+        }
+    }
+
+    OpenImagePicker = () => {
+        const options = {
+            title: 'Select picture',
+            storeOptions: {
+                skipBackuo: true,
+                path: 'images'
+            }
+        }
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            }
+            else {;
+                this.setState({
+                    imagePath: response.uri,
+                    imageHeight: response.height,
+                    imageWidth: response.width
+                });
+            }
+        });
+    }
+
     render() {
+        const { imagePath } = this.state;
         return(
             <View style={styles.container}>
-                <Text>Photo</Text>
+                {imagePath ? <Image style={styles.image} source={{uri: imagePath}}/> : null}
+                <TouchableHighlight onPress={this.OpenImagePicker}>
+                    <Text>Open Camera</Text>
+                </TouchableHighlight>
             </View>
         )
     }
@@ -21,6 +69,10 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 70,
         paddingHorizontal: 20
+    },
+    image: {
+        width: 100,
+        height: 100
     }
 });
 
