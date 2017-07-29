e338a775108adce74020db03a819f793681dc535
beto <jooseplugo@gmail.com>
2017-07-29 02:02:20 -0500

    Add google maps

---
+++ src/Components/Map.js
@@ -0,0 +1,89 @@
+//Dependencies
+import React, { Component } from 'react';
+
+import {
+    View,
+    Dimensions,
+    StyleSheet
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+class Map extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            region: {
+                latitude: null,
+                longitude: null,
+                latitudeDelta: null,
+                longitudeDelta: null
+            }
+        }
+    }
+
+    calcDelta(lat, lon, accuracy) {
+        const oneDegreeOfLogitudInMeters = 111.32;
+        const circumference = (40075 / 360);
+
+        const latDelta = accuracy * (1 / (Math.cos(lat) * circumference));
+        const lonDelta = (accuracy / oneDegreeOfLogitudInMeters);
+
+        this.setState({
+            region: {
+                latitude: lat,
+                longitude: lon,
+                latitudeDelta: latDelta,
+                longitudeDelta: lonDelta    
+            }
+        })
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                const accuracy = position.coords.accuracy;
+                this.calcDelta(lat, lon, accuracy);
+            },
+            error => console.warn(JSON.stringify(error))
+        )
+    }
+
+    marker() {
+        return{
+            latitude: this.state.region.latitude,
+            longitude: this.state.region.longitude
+        }
+    }
+
+    render() {
+        const { region } = this.state;
+        console.log(region)
+        return(
+            <View style={styles.container}>
+                {region.latitude ? <MapView
+                    style={styles.map}
+                    initialRegion={this.state.region}
+                >
+                    <MapView.Marker
+                        coordinate={this.marker()}
+                        title= "I'm here"
+                        description= "home"    
+                    />
+                </MapView>: null }
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    map: {
+        width: Dimensions.get('window').width,
+        height: 300,
+    }
+});
+
+export default Map;
--- src/Views/PetDetail.js
+++ src/Views/PetDetail.js
@@ -6,10 +6,12 @@ import {
     Image,
     StyleSheet,
     ScrollView,
-    TouchableOpacity
+    TouchableOpacity,
 } from "react-native";
 import Communications from 'react-native-communications';
 import Icon from 'react-native-vector-icons/Ionicons';
+
+import Map from '../Components/Map';
 
 class PetDetail extends Component {
 
@@ -19,60 +21,65 @@ class PetDetail extends Component {
 
     render() {
         return(
-            <ScrollView style={styles.container}>
-                <View style={styles.imageContainer}>
-                    <Image 
-                        style={styles.image}
-                        source={{uri: "https://static.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg"}} />
-                </View>
-                <View>
-                    <View style={styles.infoRow}>
-                        <View style={styles.infoData}>
-                            <Text style={styles.label}>Nombre</Text>
-                            <Text>firulais (Macho)</Text>
-                        </View>
-                        <View style={styles.infoData}>
-                            <Text style={styles.label}>Raza</Text>
-                            <Text>Mestizo/cruza</Text>
-                        </View>
-                        <View style={styles.infoData}>
-                            <Text style={styles.label}>Edad</Text>
-                            <Text>1año</Text>
-                        </View>
-                    </View> 
-                </View>
-                <View>
-                    <Text style={styles.sectionTitle}>Descripción</Text>
-                    <View style={styles.descriptionContainer}>
-                        <Text style={styles.description}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam magnam maiores animi velit asperiores ipsam dolorem eveniet vitae molestias sequi quae iusto dolor molestiae obcaecati, esse illum explicabo aliquam? Ab!
-                        </Text>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        <Image 
+                            style={styles.image}
+                            source={{uri: "https://d4n5pyzr6ibrc.cloudfront.net/media/27FB7F0C-9885-42A6-9E0C19C35242B5AC/500F9F07-C391-4312-80465035E37A92EB/thul-d927ae3d-05e8-5b64-9a51-edc20022b2d9.jpg?response-content-disposition=inline"}} />
                     </View>
-                </View>
-
-                <View>
-                    <Text style={styles.sectionTitle}>Contacto</Text>
-                    <View style={styles.infoContent}>
-                        <View style={styles.infoData}>
-                            <TouchableOpacity 
-                                style={{flexDirection: "row", alignItems: "center"}}
-                                onPress={() => this.handlePhone('8442554476', true)}>
-                                <Icon name="ios-call-outline" size={25} color="#373737" />
-                                <Text style={{marginLeft: 5}}>8442923533</Text>
-                            </TouchableOpacity>
+                    <View style={styles.paddingSection}>
+                        <View style={styles.infoRow}>
+                            <View style={styles.infoData}>
+                                <Text style={styles.label}>Nombre</Text>
+                                <Text>firulais (Macho)</Text>
+                            </View>
+                            <View style={styles.infoData}>
+                                <Text style={styles.label}>Raza</Text>
+                                <Text>Mestizo/cruza</Text>
+                            </View>
+                            <View style={styles.infoData}>
+                                <Text style={styles.label}>Edad</Text>
+                                <Text>1año</Text>
+                            </View>
+                        </View> 
+                    </View>
+                    <View style={styles.paddingSection}>
+                        <Text style={styles.sectionTitle}>Descripción</Text>
+                        <View style={styles.descriptionContainer}>
+                            <Text style={styles.description}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam magnam maiores animi velit asperiores ipsam dolorem eveniet vitae molestias sequi quae iusto dolor molestiae obcaecati, esse illum explicabo aliquam? Ab!
+                            </Text>
                         </View>
-                        <View style={styles.infoData}>
-                            <TouchableOpacity
-                                style={{flexDirection: "row", alignItems: "center"}}
-                                onPress={() => Communications.email(['josee-45@hotmail.com'],null,null,'My Subject','My body text')}>
-                                <Icon name="ios-mail-outline" size={25} color="#373737" />
-                                <Text style={{marginLeft: 5}}>josee-45@hotmail.com</Text>
-                            </TouchableOpacity>
+                    </View>
+                    <View style={styles.paddingSection}>
+                        <Text style={styles.sectionTitle}>Contacto</Text>
+                        <View style={styles.infoContent}>
+                            <View style={styles.infoData}>
+                                <TouchableOpacity 
+                                    style={{flexDirection: "row", alignItems: "center"}}
+                                    onPress={() => this.handlePhone('8442554476', true)}>
+                                    <Icon name="ios-call-outline" size={25} color="#373737" />
+                                    <Text style={{marginLeft: 5}}>8442923533</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.infoData}>
+                                <TouchableOpacity
+                                    style={{flexDirection: "row", alignItems: "center"}}
+                                    onPress={() => Communications.email(['josee-45@hotmail.com'],null,null,'My Subject','My body text')}>
+                                    <Icon name="ios-mail-outline" size={25} color="#373737" />
+                                    <Text style={{marginLeft: 5}}>josee-45@hotmail.com</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.infoData} style={{flexDirection: "row", alignItems: "center"}}>
+                                    <Icon name="logo-facebook" size={25} color="#373737" />
+                                    <Text style={{marginLeft: 5}}>BetoCordobaLugo</Text>
+                            </View>
                         </View>
-                        <View style={styles.infoData} style={{flexDirection: "row", alignItems: "center"}}>
-                                <Icon name="logo-facebook" size={25} color="#373737" />
-                                <Text style={{marginLeft: 5}}>BetoCordobaLugo</Text>
-                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.sectionTitle}>Ubicación</Text>
+                        <Map />
                     </View>
                 </View>
             </ScrollView>
@@ -82,25 +89,26 @@ class PetDetail extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         paddingTop: 70,
+    },
+    paddingSection: {
         paddingHorizontal: 20
     },
     imageContainer: {
         alignItems: "center",
-
     },
     image: {
         borderRadius: 100,
-        width: 150,
-        height: 150,
+        width: 200,
+        height: 200,
     },
     infoContent: {
         borderTopWidth: 1,
         borderBottomWidth: 1,
         backgroundColor: "white",
         borderColor: "#e0e0e0",
-        padding: 15
+        padding: 15,
+        marginBottom: 10
     },
     infoRow: {
         flexDirection: "row",
@@ -111,9 +119,6 @@ const styles = StyleSheet.create({
     },
     label: {
         color: "black"
-    },
-    description: {
-        textAlign: 'justify'
     },
     descriptionContainer: {
         backgroundColor: '#e9ebee',
