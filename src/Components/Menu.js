ab64c532fb2184fd45bfa0539840ae7688351cf1
Jose Alberto <josee-45@hotmail.com>
2017-08-30 11:46:54 -0500

    Add image and name to menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 
 import {
   View,
+  Text,
+  Image,
   StyleSheet,
 } from 'react-native';
 
-import { LoginButton } from 'react-native-fbsdk';
+// import { LoginButton } from 'react-native-fbsdk';
 
 import { Actions } from 'react-native-router-flux';
 
@@ -49,12 +51,25 @@ class Menu extends Component {
     }
 
     render() {
+      const { credentials } = this.props;
+      console.log(credentials);
       return (
         <View style={styles.menu}>
           {
+            credentials ? <View
+              style={styles.header}
+            >
+              <Image
+                source={{ uri: credentials.photoURL }}
+                style={styles.photo}
+              />
+              <Text style={{ color: '#fff' }}>{credentials.displayName}</Text>
+            </View> : null
+          }
+          {
             this.handleItems()
           }
-          <LoginButton onLogoutFinished={() => Actions.Login()} />
+          {/* <LoginButton onLogoutFinished={() => Actions.Login()} /> */}
         </View>
       );
     }
@@ -64,6 +79,18 @@ const styles = StyleSheet.create({
   menu: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 150,
+    backgroundColor: '#0091EA',
+  },
+  photo: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    marginBottom: 5,
   },
 });
 
